aaf12b9b174b54d9bda7cecb23743d746bfe259c
David <dave@webshed.org>
2013-08-21 10:08:56 +0100

    Fix jog menu

--- main.cpp
+++ main.cpp
@@ -394,34 +394,41 @@ loopexit:
 
 int jog_menu(Motor *Source, Motor *Sink)
 {
-  ITEM **jog_items;
-  MENU *jog_menu;
-  int n_choices, i;
-  ITEM *cur_item;
+  //ITEM **jog_items;
+ // MENU *jog_menu;
+  //int n_choices, i;
+  //ITEM *cur_item;
   int c, selection;
   
   /* Initialize items */
-        n_choices = ARRAY_SIZE(jogchoices);
-        jog_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
-        for(i = 0; i < n_choices; ++i)
-                jog_items[i] = new_item(jogchoices[i],jogchoices[i]);
-	jog_items[n_choices] = (ITEM *)NULL;
+  //      n_choices = ARRAY_SIZE(jogchoices);
+  //      jog_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
+  //      for(i = 0; i < n_choices; ++i)
+  //              jog_items[i] = new_item(jogchoices[i],jogchoices[i]);
+	//jog_items[n_choices] = (ITEM *)NULL;
 	//item_opts_off(main_items[3], O_SELECTABLE);
 	//item_opts_off(main_items[6], O_SELECTABLE);
 
 	/* Create main menu */
-	jog_menu = new_menu((ITEM **)jog_items);
+	//jog_menu = new_menu((ITEM **)jog_items);
 	
 	/* Set menu option not to show the description */
-	menu_opts_off(jog_menu, O_SHOWDESC);
+	//menu_opts_off(jog_menu, O_SHOWDESC);
 
 	/* Set fore ground and back ground of the menu */
-	set_menu_fore(jog_menu, COLOR_PAIR(1) | A_REVERSE);
-	set_menu_back(jog_menu, COLOR_PAIR(2));
-	set_menu_grey(jog_menu, COLOR_PAIR(3));
+	//set_menu_fore(jog_menu, COLOR_PAIR(1) | A_REVERSE);
+	//set_menu_back(jog_menu, COLOR_PAIR(2));
+	//set_menu_grey(jog_menu, COLOR_PAIR(3));
 
 	/* Post the menu */
-	mvprintw(LINES - 5, 0, "Navigate with Arrow Keys, ENTER selects");
+	//mvprintw(LINES - 5, 0, "Navigate with Arrow Keys, ENTER selects");
+	mvprintw(LINES - 10, 0, "Cursor Up = Speed Up Wire Transfer");
+	mvprintw(LINES - 9, 0, "Cursor Down = Slow Down Wire Transfer");
+	mvprintw(LINES - 8, 0, "SPACE = Emergency Stop");
+	mvprintw(LINES - 7, 0, "x = exit back to main menu");
+	
+	
+	
 	if (Source->Locked){mvprintw(LINES - 3, 0, "Motor 1 is: Locked");}
 	else
 		mvprintw(LINES - 3, 0, "Motor 1 is: Free");
@@ -430,7 +437,7 @@ int jog_menu(Motor *Source, Motor *Sink)
 	else
 		mvprintw(LINES - 2, 0, "Motor 2 is: Free");
 	mvprintw(LINES -1,0,"Source speed %d, Takeup speed %d",supplyS,takeupS);
-	post_menu(jog_menu);
+	//post_menu(jog_menu);
 	refresh();
 
 	while((c = getch()) != 88)
@@ -449,9 +456,9 @@ int jog_menu(Motor *Source, Motor *Sink)
 	}	
 outerbreak:
 
-	unpost_menu(jog_menu);
-	for(i = 0; i < n_choices; ++i)
-		free_item(jog_items[i]);
-	free_menu(jog_menu);
+	//unpost_menu(jog_menu);
+	//for(i = 0; i < n_choices; ++i)
+	//	free_item(jog_items[i]);
+	//free_menu(jog_menu);
 	return (selection);
 }
