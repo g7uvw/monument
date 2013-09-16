cc8506fec14be405de52d1e2fe1028254be0120a
David Mills <dave@wenshed.org>
2013-09-16 21:40:55 +0100

    Towards a cleaner version of the code that will be easier to maintain

---
+++ setup.cpp
@@ -0,0 +1,171 @@
+#include "hooke.h"
+// setup menu stuff
+const char *setupchoices[] = {				
+                        "Lock / Free Motor 1",
+                        "Lock / Free Motor 2",
+                        "Set Supply Spool Diameter",
+                        "Set Take-up Spool Diameter",
+						"Set Transfer Speed",
+						"Set Transfer Length",
+						"Return to Main Menu",
+                  };
+		  
+		  int setup_menu(Motor* Source, Motor* Sink)
+{
+  ITEM **setup_items;
+  MENU *setup_menu;
+  int n_choices, i;
+  ITEM *cur_item;
+  int c, selection;
+  
+  /* Initialize items */
+        n_choices = ARRAY_SIZE(setupchoices);
+        setup_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
+        for(i = 0; i < n_choices; ++i)
+                setup_items[i] = new_item(setupchoices[i],setupchoices[i]);
+	setup_items[n_choices] = (ITEM *)NULL;
+
+
+	/* Create main menu */
+	setup_menu = new_menu((ITEM **)setup_items);
+	
+	/* Set menu option not to show the description */
+	menu_opts_off(setup_menu, O_SHOWDESC);
+
+	/* Set fore ground and back ground of the menu */
+	set_menu_fore(setup_menu, COLOR_PAIR(1) | A_REVERSE);
+	set_menu_back(setup_menu, COLOR_PAIR(2));
+	set_menu_grey(setup_menu, COLOR_PAIR(3));
+
+	/* Post the menu */
+	mvprintw(LINES - 5, 0, "Navigate with Arrow Keys, ENTER selects");
+	if (Source->Locked){mvprintw(LINES - 3, 0, "Motor 1 is: Locked");}
+	else
+		mvprintw(LINES - 3, 0, "Motor 1 is: Free");
+	
+	if (Sink->Locked){mvprintw(LINES - 2, 0, "Motor 2 is: Locked");}
+	else
+		mvprintw(LINES - 2, 0, "Motor 2 is: Free");
+	mvprintw(LINES -1,0,"Source Diameter %f mm, Takeup Diameter %f mm, Speed %d mm/s",supplyD,takeupD,takeupS/1000);
+
+
+
+	post_menu(setup_menu);
+	refresh();
+
+	while((c = getch()) != KEY_F(4))
+	{       switch(c)
+	        {	case KEY_DOWN:
+				menu_driver(setup_menu, REQ_DOWN_ITEM);
+				break;
+			case KEY_UP:
+				menu_driver(setup_menu, REQ_UP_ITEM);
+				break;
+			case 10: /* Enter */
+			
+				selection = item_index(current_item(setup_menu));    
+				goto outerbreak;
+		}
+	}	
+outerbreak:
+
+	unpost_menu(setup_menu);
+	for(i = 0; i < n_choices; ++i)
+		free_item(setup_items[i]);
+	free_menu(setup_menu);
+	return (selection);
+}
+
+void dosetup(Motor *Source, Motor *Sink)
+{
+int menu_selection = -1;
+
+setup_menu_entry:
+  menu_selection = setup_menu(Source, Sink);
+  
+  switch (menu_selection)
+	{
+	  case 0:
+	  if (Source->Locked)		  
+		Source->Free();
+	  else
+		Source->Lock();
+	    goto setup_menu_entry;
+	  case 1:
+	if (Sink->Locked)
+		Sink->Free();
+	else
+		Sink->Lock();
+	    goto setup_menu_entry;
+	  case 2:
+	    clear();
+		refresh();
+		printw(". Supply spool diameter\n");
+		printw("Enter Supply spool diameter in mm:");
+		refresh();
+		cin >> supplyD;
+		printw("%f\n",supplyD);
+		refresh();
+		printw("Supply spool diameter is now: %f mm\n",supplyD);
+		printw("Press Enter to return to main menu.\n");
+		refresh();
+		getch();
+		goto setup_menu_entry;
+	    
+	  case 3:
+	    clear();
+		refresh();
+		printw(". Take-up spool diamter\n");
+		printw("Enter Take-up spool diameter in mm:");
+		refresh();
+		cin >> takeupD;
+		printw("%f\n",takeupD);
+		refresh();
+		printw("Take-up spool diameter is now: %f mm\n",takeupD);
+		printw("Press Enter to return to main menu.\n");
+		refresh(); 
+		getch();
+	    goto setup_menu_entry;
+	  case 4:
+	    clear();
+		refresh();
+		printw(". Transfer speed\n");
+		printw("WARNING : Correct spool diamters must be set first, else this speed WILL BE WRONG!\n");
+		printw("Enter transfer speed in mm/s:");
+		refresh();
+		cin >> takeupS;
+		printw("%d\n",takeupS);
+		refresh();
+		takeupC = 3.1415926535 * takeupD;
+		temp = (500/takeupC)*takeupS;
+		takeupS = temp;
+		supplyS = takeupS*(takeupD/supplyD);
+		Source->SetSpeed(supplyS);
+		Sink->SetSpeed(takeupS);
+		printw("Take-up spool speed is now %d \n",takeupS);
+		printw("Supply spool speed is now %d \n",supplyS);
+		printw("Press Enter to return to main menu.\n");
+		refresh();
+		getch();
+	    goto setup_menu_entry;
+	  case 5:
+	    clear();
+		refresh();
+		printw(". Wire Length\n");
+		printw("WARNING : Correct spool diamters must be set first, else this speed WILL BE WRONG!\n");
+		printw("Enter length of wire to transfer in mm:");
+		refresh();
+		cin >> transferLength;
+		printw("%d\n",transferLength);
+		refresh();
+		transferLength *=5000;
+		printw("Press Enter to return to main menu.\n");
+		refresh();
+		getch();
+	    goto setup_menu_entry;
+	  case 6:
+	    break;
+	    
+	}
+  
+}
---
+++ vars.cpp
@@ -0,0 +1,75 @@
+#include "hooke.h"
+
+double supplyD = 30.0;
+ double takeupD = 50.2;
+ double takeupC;// = 3.1415926535 * takeupD;
+ double supplyS = 9000;
+ double takeupS = 8000;
+ int supplyA = 100;
+ int takeupA = 100;
+ double transferLength = 1000; //mm
+ 
+void SetsupplyD (double val)
+{
+	supplyD = val;	
+}
+
+double GetsupplyD(void)
+{
+	return supplyD;
+}
+
+void SettakeupD (double val)
+{
+	takeupD = val;
+}
+
+double GettakeupD(void)
+{
+	return takeupD;
+}
+
+double GettakeupC(void)
+{
+	return 3.1415926535 * takeupD
+}
+
+void SetsupplyS(double val)
+{
+	supplyS = val;
+}
+
+double GetsupplyS(void)
+{
+	return supplyS;
+}
+
+void SettakeupS(double val)
+{
+	takeupS = val;
+}
+
+double GettakeupS(void)
+{
+	return takeupS;
+}
+
+int GettakeupA(void)
+{
+	return takeupA;
+}
+
+int GetsupplyA(void)
+{
+	return takeupA;
+}
+
+void SettransferLength(double val)
+{
+	transferLength = val;
+}
+
+double GettransferLength(void)
+{
+	return transferLength;
+}
